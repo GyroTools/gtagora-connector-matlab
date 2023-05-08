2da655de957068e390c1aee5bb791425d24b3b25
martin <martin.buehrer@gyrotools.com>
2023-05-08 13:59:15 +0200

    simplified upload

--- +agora_connector/+http/Client.m
+++ +agora_connector/+http/Client.m
@@ -28,89 +28,48 @@ classdef Client
             if nargin < 3
                 timeout = self.TIMEOUT;
             end
-            url = [self.connection.url, url];            
-            auth = self.connection.get_auth();
-            
-            if ~self.connection.verify_certificate
-                options = weboptions('CertificateFilename','');
-            else
-                options = weboptions;
-            end                               
-            options.Timeout = timeout;
-            options.MediaType = 'application/json';
-            options = auth.add(options);
-            
+
+            options = self.get_options(timeout);
+            url = self.get_url(url);                      
             response = webread(url, options);
         end
         
         function response = post(self, url, data, timeout)
+            if nargin < 3 || isempty(data)
+                data = struct;
+            end
+            if nargin < 4
+                timeout = self.TIMEOUT;
+            end
+
+            options = self.get_options(timeout);
+            url = self.get_url(url);
+            response = webwrite(url, data, options);          
+        end
+
+        function response = put(self, url, data, timeout)
             if nargin < 3
                 data = struct;
             end
             if nargin < 4
                 timeout = self.TIMEOUT;
             end
-            if isempty(data)
-                data = struct;
-            end
-            url = [self.connection.url, url];            
-            auth = self.connection.get_auth();
-            
-            if ~self.connection.verify_certificate
-                options = weboptions('CertificateFilename','');
-            else
-                options = weboptions;
-            end                               
-            options.Timeout = timeout;
-            options.MediaType = 'application/json';
-            options = auth.add(options);
-            
-            response = webwrite(url, data, options);            
+
+            options = self.get_options(timeout, 'put');
+            url = self.get_url(url);
+            response = webwrite(url, data, options);     
         end
-        
-        function response = put(self, url, data, timeout)            
+
+        function response = delete(self, url, timeout)            
             if nargin < 3
-                data = [];
-            end
-            if nargin < 4
                 timeout = self.TIMEOUT;
             end
-            
-            request = self.get_base_request(data, matlab.net.http.RequestMethod.PUT);
-            resp = self.send_request(url, request, timeout);                        
-            response = resp.Body.Data;
+
+            options = self.get_options(timeout, 'delete');
+            url = self.get_url(url);
+            response = webwrite(url, options);     
         end
-        
-        function request = get_base_request(self, data, method)
-            if nargin < 2
-                data = [];
-            end                        
-            body = matlab.net.http.MessageBody(data);
-            contentTypeField = matlab.net.http.field.ContentTypeField('application/json');
-            type = matlab.net.http.MediaType('application/json','q','.5');
-            acceptField = matlab.net.http.field.AcceptField(type);
-            
-            auth = self.connection.get_auth();
-            authField = auth.get_field();  
-                                    
-            header = [acceptField contentTypeField authField];           
-            request = matlab.net.http.RequestMessage(method,header,body);                        
-        end
-        
-        function resp = send_request(self, url, request, timeout)            
-            if nargin < 4
-                timeout = self.TIMEOUT;
-            end            
-            options = matlab.net.http.HTTPOptions('ConnectTimeout', timeout);                        
-            uri = matlab.net.URI([self.connection.url, url]);                                                                       
-            resp = request.send(uri, options);
-                                    
-            sc = resp.StatusCode;
-            if sc ~= matlab.net.http.StatusCode.OK
-                error([getReasonPhrase(getClass(sc)),': ',getReasonPhrase(sc)])                
-            end                        
-        end
-        
+                               
         function download(self, url, target_filename)            
             url = [self.connection.url, url];            
             auth = self.connection.get_auth();
@@ -125,18 +84,135 @@ classdef Client
             websave(target_filename, url, options);
         end
 
-        function response = upload(self, url, input_files, target_files, timeout) 
-            import agora_connector.http.ConnectionLegacy
-            import agora_connector.http.ClientLegacy
+%         function response = upload(self, url, input_files, target_files, timeout) 
+%             import agora_connector.http.ConnectionLegacy
+%             import agora_connector.http.ClientLegacy
+% 
+%             if nargin < 5
+%                 timeout = [];
+%             end
+% 
+%             auth = self.connection.get_auth();
+%             full_url = [self.connection.url, url];
+%             connections_legacy = ConnectionLegacy(self.connection.url, auth.api_key);
+%             response = ClientLegacy.Upload(full_url, input_files, target_files, connections_legacy, timeout);
+%         end
 
+        function upload(self, url, input_files, target_files, timeout)             
             if nargin < 5
-                timeout = [];
+                timeout = self.TIMEOUT;
             end
+                                   
+            if ~iscell(input_files)
+                input_files = {input_files};
+            end
+            if ~iscell(target_files)
+                target_files = {target_files};
+            end
+            if length(target_files) ~= length(input_files)
+                error('input_files and target_files must have the same length');
+            end
+            url = self.get_url(url);
+            chunk_size = self.get_upload_chunk_size();    
 
-            auth = self.connection.get_auth();
-            full_url = [self.connection.url, url];
-            connections_legacy = ConnectionLegacy(self.connection.url, auth.api_key);
-            response = ClientLegacy.Upload(full_url, input_files, target_files, connections_legacy, timeout);
+            BOUNDARY = '***********************';
+            EOL = sprintf('%s%s',[13, 10]);
+            content_type_field = matlab.net.http.field.ContentTypeField(matlab.net.http.MediaType('multipart/form-data', 'boundary', BOUNDARY));
+            auth_field = self.connection.get_auth().get_field();
+            type = matlab.net.http.MediaType('application/json');
+            acceptField = matlab.net.http.field.AcceptField(type);
+            header = [content_type_field, auth_field, acceptField];
+            uri = matlab.net.URI(url);
+            
+            for i = 1:length(input_files)                
+                fid = fopen(input_files{i});
+                if fid == -1                    
+                    error(['Cannot open file = ', input_files{i}]);
+                end
+                fseek(fid, 0, 'eof');
+                filesize = ftell(fid);
+                fseek(fid, 0, 'bof');
+                nr_chunks = ceil(filesize/chunk_size);
+                                
+                filename = target_files{i};
+                uid = char(java.util.UUID.randomUUID);                              
+                    
+                for cur_chunk = 1:nr_chunks                   
+                    d = fread(fid,chunk_size,'*uint8'); % Read in byte stream    
+
+                    body=struct;
+                    body.description = '';
+                    body.flowChunkNumber = num2str(cur_chunk);
+                    body.flowChunkSize = num2str(chunk_size);
+                    body.flowCurrentChunkSize = num2str(length(d));
+                    body.flowTotalSize = num2str(filesize);
+                    body.flowIdentifier = uid;
+                    body.flowFilename = filename;
+                    body.flowRelativePath = filename;
+                    body.flowTotalChunks = num2str(nr_chunks); 
+                    
+                    % the following code is probably a bit too complicated
+                    % but it's the only way I found which worked.
+                    % The workflow is the following:
+                    %   1. create a string called data
+                    %   2. add the form fields as string to data
+                    %   3. convert the string to binary (uint8) and add the
+                    %      file chunk as binary
+                    %   4. finalize the data with the termination string
+                    %   5. create a MessageBody object with the binary data
+                    %   6. create a request with the body and send it
+                    data = '';
+                    fn = fieldnames(body);
+                    for j = 1:length(fn)
+                        data = [data, ...
+                            '--', BOUNDARY, EOL, ...
+                            'Content-Disposition: form-data; name="',fn{j},'"', EOL, EOL, ...
+                            body.(fn{j}), EOL ];
+                    end
+                    data = [data, ...
+                        '--', BOUNDARY, EOL,...
+                        'Content-Disposition: form-data; name="file"; filename="', filename, '"', EOL,...
+                        'Content-Type: application/octet-stream',EOL,...
+                        EOL];
+
+                    data = [uint8(data), d'];
+                    finalize_str = [EOL, '--', BOUNDARY, '--', EOL];
+                    data = [data, uint8(finalize_str)];
+                    body = matlab.net.http.MessageBody;
+                    body.Payload = data;
+
+                    req = matlab.net.http.RequestMessage('POST', header, body);
+                    options = matlab.net.http.HTTPOptions('ConnectTimeout', timeout);
+                    req.send(uri, options);
+                end
+                fclose(fid);
+            end                     
+        end
+    end
+
+    methods (Hidden)        
+        function options = get_options(self, timeout, type)                                  
+            if nargin < 2
+                timeout = self.TIMEOUT;
+            end
+            if nargin < 3
+                type = [];
+            end              
+            auth = self.connection.get_auth();            
+            options = weboptions;
+            if ~self.connection.verify_certificate
+                options.CertificateFilename = '';            
+            end     
+            if ~isempty(type)
+                options.RequestMethod = type;
+            end
+            options.Timeout = timeout;
+            options.MediaType = 'application/json';
+            options = auth.add(options);                                       
+        end
+
+        function url = get_url(self, url)
+            url = [self.connection.url, url];    
         end
     end
 
@@ -151,7 +227,7 @@ classdef Client
             else
                 chunk_size = 100*1024*1024;
             end
-        end
+        end        
     end
 end
 
